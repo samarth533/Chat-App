242c1ee07105f1918858c5222924643ae0d88eb0
Samarth <samarth@grosamriddhi.com>
2022-09-08 12:44:52 +0530

    Added a Procfile.

---
+++ public/js/client-copy.js
@@ -0,0 +1,71 @@
+const { append } = require("express/lib/response");
+
+const socket = io();
+
+var username;
+
+var chats = document.querySelector(".chats");
+var users_list = document.querySelector(".users-list");
+var users_count = document.querySelector(".users-count");
+var msg_send = document.querySelector("#user-send");
+var user_msg = document.querySelector("#user-msg");
+
+do{    
+    username=prompt("Enter Username : ");
+}while(!username);
+
+/* IT WILL BE CALLED WHEN NEW USER JOINS*/
+socket.emit("new-user-joined",username);
+
+socket.on("user-connected",(socket_name)=>{
+    userJoinLeft(socket_name,'joined');
+});
+
+/*FUNCTION TO CREATE JOIN.LEFT STATUS DIV */
+function userJoinLeft(name,status){
+    let div = document.createElement("div");
+    div.classList.add('user-join');
+    let content = `<p><b>${name}</b> ${status} the chat</p>`;
+    div.innerHTML=content;
+    chats.appendChild(div);
+}   
+
+socket.on("user-disconnected",(user)=>{
+    userJoinLeft(user,'left');
+});
+
+/* TO UPDATE USER LIST AND USER COUNT */
+socket.on('user-list',(users)=>{
+    users_list.innerHTML="";
+    users_arr=Object.values(users);
+    for(i=0;i<users_arr.length;i++){
+        let p=document.createElement('p');
+        p.innerText=users_arr[i];
+        users_list.appendChild(p);
+    }
+    users_count.innerHTML=users_arr.length;
+});
+
+/*For Sending Messages */
+
+msg_send.addEventListener('click',()=>{
+    let data={
+        user: username,
+        msg: user_msg.value
+    };
+    if(user_msg.value!=''){
+        appendMessage(data,'outgoing');
+        socket.emit('message',data);
+        user_msg.value='';
+    }
+});
+
+function appendMessage(data,status){
+    let div=document.createElement('div');
+    div.classlist.add('message',status);
+    let content=`
+    <h5>${data.user}</h5>
+    <p>${data.msg}</p>`;
+    div.innerHTML=content;
+    chats.appendChild(div);
+}
